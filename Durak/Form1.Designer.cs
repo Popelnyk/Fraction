5b86fd62ea2d7cbe5e689f671a98dbbae57282ca
Alexander Popelnyk <alexander.popelnyk@banuba.com>
2021-05-17 23:08:24 +0300

    git init

---
+++ Durak/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Durak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.Location = new System.Drawing.Point(496, 261);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(95, 42);
+            this.buttonNo.TabIndex = 0;
+            this.buttonNo.TabStop = false;
+            this.buttonNo.Text = "Нет";
+            this.buttonNo.UseVisualStyleBackColor = true;
+            this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
+            this.buttonNo.MouseHover += new System.EventHandler(this.buttonNo_MouseHover);
+            this.buttonNo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.buttonNo_MouseMove);
+            this.buttonNo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.buttonNo_MouseUp);
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.buttonYes.Location = new System.Drawing.Point(484, 35);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(156, 98);
+            this.buttonYes.TabIndex = 1;
+            this.buttonYes.Text = "Да";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(123, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ты дурак?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(607, 469);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BackgroundImage = global::Durak.Properties.Resources.рыбка;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonYes);
+            this.Controls.Add(this.buttonNo);
+            this.MinimumSize = new System.Drawing.Size(750, 550);
+            this.Name = "Form1";
+            this.Text = "Дурак";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNo;
+        private System.Windows.Forms.Button buttonYes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Durak/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Durak
+{
+    public partial class Form1 : Form
+    {
+        string cheat;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void borders() 
+        {
+            Point m = PointToClient(Cursor.Position);
+            Point degr = buttonNo.Location;
+            Point l = label1.Location;
+            Point Yes = buttonYes.Location;
+            Point TB = textBox1.Location;
+            int x = degr.X;
+            int y = degr.Y;
+            
+        }
+        private void buttonNo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (cheat != "Пузырек") {
+                Point m = PointToClient(Cursor.Position);
+                Point degr = buttonNo.Location;
+                Point l = label1.Location;
+                Point Yes = buttonYes.Location;
+                Point TB = textBox1.Location;
+                int x = degr.X;
+                int y = degr.Y;
+                if (m.X < degr.X + 10)
+                {
+                    x += 10;
+                }
+                if (m.Y < degr.Y + 10)
+                {
+                    y += 10;
+                }
+                if (m.X > degr.X +buttonNo.Width - 10)
+                {
+                    x -= 10;
+                }
+                if (m.Y > degr.Y + buttonNo.Height - 10)
+                {
+                    y -= 10;
+                }
+                if (x < 15)
+                {
+                    x += 50;
+                }
+                if (x + buttonNo.Width > Width - 25)
+                {
+                    x -= 50;
+                }
+                if (y < 25)
+                {
+                    y += 50;
+                }
+                if (y + buttonNo.Height > Height - 35)
+                {
+                    y -= 50;
+                }
+                if ((x >= l.X - buttonNo.Width && x <= (l.X + label1.Width)) && (y >= l.Y - buttonNo.Width && y <= (l.Y + label1.Height)))
+                {
+                    x = Width - 400;
+                    y = Height - 200;
+                }
+                if ((x >= Yes.X - buttonNo.Width && x <= (Yes.X + buttonYes.Width)) && (y >= Yes.Y - buttonNo.Height && y <= (Yes.Y + buttonYes.Height)))
+                {
+                    x = Width - 400;
+                    y = Height - 200;
+                }
+                if ((x >= TB.X - buttonNo.Width && x <= (TB.X + textBox1.Width)) && (y >= TB.Y - buttonNo.Height && y <= (TB.Y + textBox1.Height)))
+                {
+                    x = Width - 400;
+                    y = Height - 200;
+                }
+                buttonNo.Location = new Point(x, y);
+            }
+        }
+
+        private void buttonYes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы дурак!", "Поздравляем");
+        }
+
+        private void buttonNo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ты не дурак!");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            cheat = textBox1.Text;
+        }
+
+        private void buttonNo_MouseUp(object sender, MouseEventArgs e)
+        {
+      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (cheat != "Пузырек")
+            {
+                Point m = PointToClient(Cursor.Position);
+                Point degr = buttonNo.Location;
+                Point l = label1.Location;
+                Point Yes = buttonYes.Location;
+                Point TB = textBox1.Location;
+                if (m.X >= degr.X && m.X <= degr.X + buttonNo.Width && m.Y >= degr.Y && m.Y <= degr.Y + buttonNo.Height)
+                {
+                    int x = degr.X + buttonNo.Width;
+                    int y = degr.Y + buttonNo.Height;
+                    if (x < 15)
+                    {
+                        x += 100;
+                    }
+                    if (x + buttonNo.Width > Width - 25)
+                    {
+                        x -= 100;
+                    }
+                    if (y < 25)
+                    {
+                        y += 40;
+                    }
+                    if (y + buttonNo.Height > Height - 25)
+                    {
+                        y -= 40;
+                    }
+                    if ((x >= l.X - buttonNo.Width && x <= (l.X + label1.Width)) && (y >= l.Y - buttonNo.Width && y <= (l.Y + label1.Height)))
+                    {
+                        x = Width - 400;
+                        y = Height - 200;
+                    }
+                    if ((x >= Yes.X - buttonNo.Width && x <= (Yes.X + buttonYes.Width)) && (y >= Yes.Y - buttonNo.Height && y <= (Yes.Y + buttonYes.Height)))
+                    {
+                        x = Width - 400;
+                        y = Height - 200;
+                    }
+                    if ((x >= TB.X - buttonNo.Width && x <= (TB.X + textBox1.Width)) && (y >= TB.Y - buttonNo.Height && y <= (TB.Y + textBox1.Height)))
+                    {
+                        x = Width - 400;
+                        y = Height - 200;
+                    }
+                    buttonNo.Location = new Point(x, y);
+                }
+            }
+        }
+
+        private void buttonNo_MouseHover(object sender, EventArgs e)
+        {
+            if (cheat != "Пузырек")
+            {
+                Point m = PointToClient(Cursor.Position);
+                Point degr = buttonNo.Location;
+                Point l = label1.Location;
+                Point Yes = buttonYes.Location;
+                Point TB = textBox1.Location;
+                if (m.X >= degr.X && m.X <= degr.X + buttonNo.Width && m.Y >= degr.Y && m.Y <= degr.Y + buttonNo.Height)
+                {
+                    int x = degr.X + buttonNo.Width;
+                    int y = degr.Y + buttonNo.Height;
+                    if (x < 15)
+                    {
+                        x += 100;
+                    }
+                    if (x + buttonNo.Width > Width - 25)
+                    {
+                        x -= 100;
+                    }
+                    if (y < 25)
+                    {
+                        y += 40;
+                    }
+                    if (y + buttonNo.Height > Height - 25)
+                    {
+                        y -= 40;
+                    }
+                    if ((x >= l.X - buttonNo.Width && x <= (l.X + label1.Width)) && (y >= l.Y - buttonNo.Width && y <= (l.Y + label1.Height)))
+                    {
+                        x = Width - 400;
+                        y = Height - 200;
+                    }
+                    if ((x >= Yes.X - buttonNo.Width && x <= (Yes.X + buttonYes.Width)) && (y >= Yes.Y - buttonNo.Height && y <= (Yes.Y + buttonYes.Height)))
+                    {
+                        x = Width - 400;
+                        y = Height - 200;
+                    }
+                    if ((x >= TB.X - buttonNo.Width && x <= (TB.X + textBox1.Width)) && (y >= TB.Y - buttonNo.Height && y <= (TB.Y + textBox1.Height)))
+                    {
+                        x = Width - 400;
+                        y = Height - 200;
+                    }
+                    buttonNo.Location = new Point(x, y);
+                }
+            }
+        }
+    }
+}
